84db40ee1d65e935806ee685a5f7825fea16581f
cakasuma <amammustofa@gmail.com>
2019-01-13 17:47:33 +0700

    finish user service and configure error handling

---
+++ _helpers/jwt.js
@@ -0,0 +1,21 @@
+import expressJwt from "express-jwt";
+import config from "config.json";
+import userService from "../users/user.service";
+
+const isRevoked = async (req, payload, done) => {
+  const user = await userService.getById(payload.sub);
+
+  if (!user) {
+    return done(null, true);
+  }
+
+  done();
+};
+
+export const jwt = () => {
+  const secret = config.secret;
+
+  return expressJwt({ secret, isRevoked }).unless({
+    path: ["/users/authenticate", "/users/register"]
+  });
+};
